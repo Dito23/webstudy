95d23aff04443333842f8a15beb1495985bff205
Dito23 <dimitri.bolokadze@gmail.com>
2025-04-09 13:04:30 -0700

    sddsqewq

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -14,7 +14,7 @@ function SignIn({ onSignIn }) {
 
   return (
     <div className="sign-in">
-      <h2>sign In Please</h2>
+      <h2>sign In IN IN IN </h2>
       <form>
         <GoogleLogin
           onSuccess={handleLoginSuccess}
